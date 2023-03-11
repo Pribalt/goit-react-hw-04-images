f35d793192329996cc57128de6fde658aa7704d0
Pribalt <vitalisvitalik@gmail.com>
2023-03-12 01:57:37 +0200

    fix app and Searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,38 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { GlobalStyle } from 'components/GlobalStyle';
 import { AppStyled } from 'components/App.styled';
 import { Searchbar } from 'components/Searchbar/Searchbar';
 import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 
-export class App extends Component {
-  state = {
-    textSearch: '',
-    page: 1,
+export const App = () => {
+  const [textSearch, setTextSearch] = useState('');
+  const [page, setPage] = useState(1);
+
+  const handleSubmit = textSearch => {
+    setTextSearch(textSearch);
+    setPage(1);
   };
 
-  handleSubmit = textSearch => {
-    this.setState({ textSearch, page: 1 });
+  const handleLoad = () => {
+    setPage(page + 1);
   };
 
-  handleLoad = () => {
-    this.setState(({ page }) => ({ page: page + 1 }));
-  };
-
-  render() {
-    const { textSearch, page } = this.state;
-    const { handleSubmit, handleLoad } = this;
-
-    return (
-      <AppStyled>
-        <GlobalStyle />
-        <Toaster
-          position="top-right"
-          toastOptions={{
-            duration: 2000,
-          }}
-        />
-        <Searchbar onSearch={handleSubmit} />
-        <ImageGallery
-          textSearch={textSearch}
-          handleLoad={handleLoad}
-          page={page}
-        />
-      </AppStyled>
-    );
-  }
-}
+  return (
+    <AppStyled>
+      <GlobalStyle />
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 2000,
+        }}
+      />
+      <Searchbar onSearch={handleSubmit} />
+      <ImageGallery
+        textSearch={textSearch}
+        handleLoad={handleLoad}
+        page={page}
+      />
+    </AppStyled>
+  );
+};
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import { toast } from 'react-hot-toast';
 import PropTypes from 'prop-types';
@@ -10,52 +10,45 @@ import {
   Input,
 } from 'components/Searchbar/Searchbar.styled';
 
-export class Searchbar extends Component {
-  state = {
-    value: '',
+export const Searchbar = ({ onSearch }) => {
+  const [value, setValue] = useState('');
+
+  const handleChange = ({ target: { value } }) => {
+    setValue(value);
   };
 
-  handleChange = ({ target: { value } }) => {
-    this.setState({ value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    if (!this.state.value.trim()) {
+    if (!value.trim()) {
       toast.error('The search field is empty!');
       return;
     }
 
-    this.props.onSearch(this.state.value);
-    this.setState({ value: '' });
+    onSearch(value);
+    setValue('');
   };
 
-  render() {
-    const { value } = this.state;
-    const { handleSubmit, handleChange } = this;
+  return (
+    <Header>
+      <Form onSubmit={handleSubmit}>
+        <Button type="submit">
+          <BsSearch />
+          <Span>Search</Span>
+        </Button>
 
-    return (
-      <Header>
-        <Form onSubmit={handleSubmit}>
-          <Button type="submit">
-            <BsSearch />
-            <Span>Search</Span>
-          </Button>
-
-          <Input
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={value}
-            onChange={handleChange}
-          />
-        </Form>
-      </Header>
-    );
-  }
-}
+        <Input
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={value}
+          onChange={handleChange}
+        />
+      </Form>
+    </Header>
+  );
+};
 
 Searchbar.propTypes = {
   onSearch: PropTypes.func.isRequired,
